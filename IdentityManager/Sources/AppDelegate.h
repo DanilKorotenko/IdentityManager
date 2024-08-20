ee9c06c528f748c47816b35f2a7a7d0532e12959
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-20 17:21:39 +0300

    Initial commit.

---
+++ IdentityManager/Sources/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IdentityManager
+//
+//  Created by Danil Korotenko on 8/20/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
