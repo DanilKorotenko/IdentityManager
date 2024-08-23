bc5dbc0537ec4253c5c4798b0718b030118b9eef
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-23 12:59:18 +0300

    User list view added.

---
+++ IdentityManager/Sources/UsersList.h
@@ -0,0 +1,16 @@
+//
+//  UsersList.h
+//  IdentityManager
+//
+//  Created by Danil Korotenko on 8/20/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UsersList : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IdentityManager/Sources/UsersList.m
@@ -0,0 +1,22 @@
+//
+//  UsersList.m
+//  IdentityManager
+//
+//  Created by Danil Korotenko on 8/20/24.
+//
+
+#import "UsersList.h"
+
+@interface UsersList ()
+
+@end
+
+@implementation UsersList
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
