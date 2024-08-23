8495cf39a1bbf082e29334819def8c66ed3bed4c
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-23 13:56:09 +0300

    Added identity utilities.

--- IdentityManager/Sources/UsersList.m
+++ IdentityManager/Sources/UsersList.m
@@ -7,7 +7,13 @@
 
 #import "UsersList.h"
 
+#import "IdentityUtilities/IUIdentityQuery.h"
+
 @interface UsersList ()
+
+@property (strong) IBOutlet NSTableView *usersTable;
+
+@property (strong) IUIdentityQuery *query;
 
 @end
 
@@ -26,10 +32,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    self.query = [[IUIdentityQuery alloc] init];
+    [self.query startWithEventBlock:
+        ^(CSIdentityQueryEvent event, NSError * _Nonnull anError)
+        {
+            [self.usersTable reloadData];
+        }];
 }
 
 #pragma mark table
 
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return self.query.identities.count;
+}
 
 @end
